7c526df962d5a2d4bed3049fea7ba837f927f2f8
tilisma <tilisma123.pahari@gmail.com>
2023-12-12 08:39:21 +0545

    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMC Ecommerce</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js']) 
+</head>
+<body>
+    <h1 class="myheading">This is Homepage</h1>
+    <h1 class="font-bold bg-red-400 text-white text-xl p-7">This is new heading</h1> 
+</body>
+</html>
