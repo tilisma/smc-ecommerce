c1f853d3159fd5fc41eacff675334cfd7deb5eec
tilisma <tilisma123.pahari@gmail.com>
2024-01-31 08:32:33 +0545

    Products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request,$id)
+    {
+
+    }
+    public function edit($id){
+
+    } 
+
+    public function update(Request $request,$id )
+    {
+
+    }
+
+    public function delete($id)
+    {
+        
+    }
+}
